c848e3be4c45a838305c5cc0ab662c309b9bb19e
llmstudy831 <llmstudy831@gmail.com>
2025-06-20 17:27:24 +0900

    Update main_02.dart

--- lib/main_02.dart
+++ lib/main_02.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',  // 앱 제목(식별자 용도)
+      home: MyHomePage(),    // 앱이 시작할 때 표시할 화면 위젯 지정
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(title: Text('테이블 UI 예제')),  // 화면 상단 제목 표시줄
+      body: Container(
+        color: Colors.blue[100],             // 연한 파란색 배경을 가진 Container
+        child: Column(
+          children: [
+            Text('첫 번째 줄'),       // 첫 번째 텍스트 위젯
+            Text('두 번째 줄'),       // 두 번째 텍스트 위젯
+            Text('세 번째 줄'),       // 세 번째 텍스트 위젯
+          ],
+        )
+      ),
+    );
+
+  }
+}
